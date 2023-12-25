a24c889b46a50cf932d03dbd3387941525ef9c03
ehsan2793 <ehsan2793@gmail.com>
2023-12-24 18:26:14 -0800

    add func addNewLaunch

--- src/models/lunches.model.js
+++ src/models/lunches.model.js
@@ -1,6 +1,7 @@
 const launches = new Map();
+let latestFlightNumber = 100;
 
-const lunch = {
+const launch = {
   flightNumber: 100,
   mission: "Explorer",
   rocket: "1ST",
@@ -11,12 +12,26 @@ const lunch = {
   success: true,
 };
 
-launches.set(lunch.flightNumber, lunch);
+launches.set(launch.flightNumber, launch);
 
 function getAllLaunches() {
   return Array.from(launches.values());
 }
 
+function addNewLaunch(launch) {
+  latestFlightNumber += 1;
+  launches.set(
+    latestFlightNumber,
+    Object.assign(launch, {
+      customers: ["ehsan", "arsalan"],
+      flightNumber: latestFlightNumber,
+      upcoming: true,
+      success: true,
+    })
+  );
+}
+
 module.exports = {
   getAllLaunches,
+  addNewLaunch,
 };
